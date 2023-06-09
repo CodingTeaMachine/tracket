d5132712adbe72a2d8abc4937bd1bc2ee8407da4
Dékány Márk <h044304@stud.u-szeged.hu>
2023-06-09 20:04:42 +0200

    setup tailwindCSS and SkeletonUI

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		// eslint-disable-next-line @typescript-eslint/no-var-requires
+		require('path').join(require.resolve(
+				'@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}'
+		)
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		// eslint-disable-next-line @typescript-eslint/no-var-requires
+		...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()
+	],
+}
