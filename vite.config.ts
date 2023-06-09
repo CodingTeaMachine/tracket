a6a186782ba95617206b4363b87f32ffd8fdd683
Dékány Márk <h044304@stud.u-szeged.hu>
2023-06-09 20:05:01 +0200

    file aliases the for i18n folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	server: {
+		host: 'tracket.local',
+		port: 80
+	},
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+		include: ['src/tests/**/*.{test,spec}.{js,ts}']
+	},
+	resolve: {
+		alias: {
+			$: resolve('./src'),
+			$i18n: resolve('./src/lib/i18n')
+		},
+	},
 });
