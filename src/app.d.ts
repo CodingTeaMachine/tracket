ecc48c199d0b7b1cbb33b6b79c02fa072fd90203
Dékány Márk <codingteamachine@gmail.com>
2023-06-11 15:54:43 +0200

    fix

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,7 +1,7 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 
-import {Redirect as RedirectEnum} from "$types/HTTP";
+import {RedirectStatusCode as RedirectEnum} from "$types/HTTP";
 
 /// <reference types="@sveltejs/kit" />
 declare global {
